03b8c2a6716eaf0a741cc258d879dd4af6a5f828
unknown <Administrator@s-buildhub1.persistent.co.in>
2015-04-24 15:47:35 +0530

    first commit

---
+++ randomGen.php
@@ -0,0 +1,5 @@
+<?php
+
+function randonGen(){
+	return rand(55,98);
+}
---
+++ tests/TestCaseTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__.'/../randomGen.php';
+
+class TestCaseTest extends PHPUnit_Framework_TestCase{
+
+	public function testAvailableFileType(){
+		$randomNum = randonGen();
+		$this->assertTrue($randomNum <>50);
+	}
+}
